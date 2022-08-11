328930290d981706f5da533b49b8c2b4f1b7bcc9
oprahca <oprah8674@gmail.com>
2022-08-11 15:01:48 -0700

    Updated the back-end link via Vercel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-import BooksList from "./Components/books-list";
-import EditBook from "./Components/edit-book";
-import CreateBook from "./Components/create-book";
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-        <Route path="/" exact  element={<BooksList />} />
-        <Route path="/create" element={<CreateBook />} />
-        <Route path="/update/:id" element={<EditBook />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
