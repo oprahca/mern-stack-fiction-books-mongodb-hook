c812de3e61597786257f63a20fbe47b632666fd0
oprahca <oprah8674@gmail.com>
2022-08-11 15:05:08 -0700

    Update navbar.js

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="navbar-brand mx-3">TodoListApp</Link>{' '}
+      <Link to="/" className="navbar-brand mx-3">FinctionBooks</Link>{' '}
       <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-            <Link to="/" className="nav-link mx-1">Tasks List</Link>
+            <Link to="/" className="nav-link mx-1">Books List</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/create" className="nav-link mx-1">Add New Task</Link>
+            <Link to="/create" className="nav-link mx-1">Add New Book</Link>
           </li>
         </ul>
       </div>
